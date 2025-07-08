d1cc1ef8cbf04e83734494114f6c33157ada953b
Tanveer Inamdar <tinamdar@ttu.edu>
2025-07-07 22:01:17 -0500

    Todo query and session state

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 import os
@@ -22,11 +22,18 @@ class Todo(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        pass
-    else:
-        pass
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
 
-    return render_template('index.html')
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue 404'
+    else:
+        return render_template("index.html")
+
 
 if __name__ == "__main__":
     with app.app_context():
